2d763c4c841769d520e4207eec7e283d52c38972
Spolarium75 <mangubatreylan1@gmail.com>
2026-03-28 21:51:21 +0800

    Added +2 minutes for better accuracy

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ function formatTime(totalSeconds) {
     const safeSeconds = Math.max(0, Number(totalSeconds) || 0);
 
     const h = Math.floor(safeSeconds / 3600);
-    const m = Math.floor((safeSeconds % 3600) + 120 / 60);
+    const m = Math.floor((safeSeconds % 3600) / 60);
     const s = safeSeconds % 60;
     return { h, m, s };
 }
