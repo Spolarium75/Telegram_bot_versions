d2d271c5de39bf5db8e4a9097c5ddd094a0417bd
Spolarium75 <mangubatreylan1@gmail.com>
2026-06-05 11:45:35 +0800

    some changes made

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: '8631883429:AAFsxOmEVZrjs53XdZXhpdTuxynDDARjpJ8',
-    reminders: [1800, 1200, 600, 300, 60, 10] // 30m,20m,10m,5m,1m,10s
-};
+    token: '8680718017:AAHqfzMWH_oVrHUPSdR9gyX8M3N0dkS6cDs',
+    reminders: [1800, 1200, 600, 300, 60, 10]
+};  
